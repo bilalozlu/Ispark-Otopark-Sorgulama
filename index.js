dbf2f23f1460e943095c8b4a3c154a4dc4248db7
Bilal Özlü <bilal5071@hotmail.com>
2020-03-01 23:38:48 +0300

    trial

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
-
+const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
+var express = require('express');
+var app = express();
 
-const http = require('http');
-var request = require("request");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-http.createServer(function(req, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-    request('http://www.google.com', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body) // Print the google web page.
+  var request=require('request');
+
+  request.get('https://api.ibb.gov.tr/ispark/Park',async (request,response){
+      try {
+          const b = await response
+          res.write(b);
       }
+      catch {
+      }
+  });
+
+  res.end();
+});
+
+app.use(express.static('public'));
+app.listen(8080, () => {
+  console.log('listening on 8080');
+});
+
+
+const request = require('request');
+
+
+
+
+
+function getjsonlist(url)
+{
+  return new Promise((resolve,reject)=>
+  {
+    var options =
+    {
+      method:'GET'
+     ,url: url
+     ,agentOptions: {
+        rejectUnauthorized: false
+      }
+     ,headers:
+      {
+      }
+
+    };
+    request(options,function(error,response,body)
+    {
+      if(error)
+        reject(error);
+      var bd = JSON.parse(body);
+      resolve(bd);
     })
-  response.end();
-}).listen(8888);
+  })
+}
